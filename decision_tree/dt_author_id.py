7cab2b1530088691f507ab3590f458db3df62391
knagar757 <knagar757@gmail.com>
2018-04-13 00:14:27 +0530

    decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,20 +12,21 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.metrics import accuracy_score
+from sklearn import tree
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
+accuracy_at_min_split_40 = accuracy_score(labels_test, pred) 
 #########################################################
-
+print "accuracy at min split 40 is:", accuracy_at_min_split_40
 
